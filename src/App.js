64522e59adf91be779fc77bbffb9dfc61a99110a
Nate <nathanestes@gmail.com>
2018-09-29 15:46:21 -0600

    initial, and some troubleshooting

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Card from './Card'
+
+class App extends React.Component {
+
+state = {
+  Deck: [
+    { id: 1, front: 'front', back: 'back' },
+    { id: 2, front: 'front2', back: 'back'},
+    { id: 3, front: 'front3', back: 'back'},
+    { id: 4, front: 'front4', back: 'back'},
+  ]
+}
+
+displayCards() {
+  return this.state.Deck.map(card => {
+    return <button onClick = {() => this.flipCard.card }>
+    {card.front}
+    </button>
+  })
+}
+
+render() {
+  return(
+    <div>
+      <ul>
+        <Deck info={this.state.Deck[0]} />
+        {
+          this.displayCards
+        }
+      </ul>
+    </div>
+  )
+}
+
+}
+
+
+export default App;
